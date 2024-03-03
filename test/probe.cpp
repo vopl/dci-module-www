a4af6b5ed4c33581a8b7b847b29a5d495c880bcd
vopl <vopl@bk.ru>
2024-03-03 20:52:51 +0300

    smoke http test draft

--- test/probe.cpp
+++ test/probe.cpp
@@ -8,6 +8,7 @@
 #include <dci/test.hpp>
 #include <dci/host.hpp>
 #include <dci/poll.hpp>
+#include <dci/cmt.hpp>
 #include "www.hpp"
 
 using namespace dci;
@@ -27,17 +28,112 @@ TEST(module_www, probe)
 
     www::Factory<> wwwFactory = manager->createService<www::Factory<>>().value();
 
-    dci::sbs::Owner tol;
+    dci::sbs::Owner sol;
     www::http::server::Channel<> httpServerChannel;
-    netServer->accepted().connect(tol, [&](net::stream::Channel<> netStreamChannel)
+    netServer->accepted().connect(sol, [&](net::stream::Channel<> netStreamChannel)
     {
         httpServerChannel = wwwFactory->httpServerChannel(std::move(netStreamChannel)).value();
     });
 
-    www::http::client::Channel<> netClientChannel = netClient->connect(netServer->localEndpoint().value()).value();
-    www::http::server::Channel<> httpClientChannel = wwwFactory->httpClientChannel(std::move(netClientChannel)).value();
+    idl::net::stream::Channel<> netStreamChannel = netClient->connect(netServer->localEndpoint().value()).value();
+    www::http::client::Channel<> httpClientChannel = wwwFactory->httpClientChannel(std::move(netStreamChannel)).value();
 
+    www::http::client::Request<> request{idl::interface::Initializer{}};
+    www::http::client::Response<> response{idl::interface::Initializer{}};
+    httpClientChannel->io(request, response);
 
+    cmt::Event firstLineEvt;
+    int firstLineCounter = 0;
+    response->firstLine() += sol * [&](www::http::firstLine::Version version, primitives::uint8 statusCode, primitives::String statusText)
+    {
+        EXPECT_EQ(version, www::http::firstLine::Version::HTTP_1_1);
+        EXPECT_EQ(statusCode, 200);
+        EXPECT_EQ(statusText, "OK");
+        ++firstLineCounter;
+        firstLineEvt.raise();
+    };
 
+    cmt::Event headersEvt;
+    std::size_t headersCounter = 0;
+    response->headers() += sol * [&](primitives::List<www::http::Header> headers, bool done)
+    {
+        std::size_t localIdx{};
+        while(headersCounter < headers.size())
+        {
+            switch(headersCounter)
+            {
+            case 0:
+                EXPECT_EQ(headers[localIdx].key, www::http::header::KeyRecognized::Host);
+                EXPECT_EQ(headers[localIdx].value, "localhost");
+                break;
+            case 1:
+                EXPECT_EQ(headers[localIdx].key, "x-my-header");
+                EXPECT_EQ(headers[localIdx].value, "x-my-value");
+                break;
+            default:
+                ADD_FAILURE();
+            }
 
+            ++localIdx;
+            ++headersCounter;
+            EXPECT_EQ(done, headersCounter == 2);
+            if(done)
+                headersEvt.raise();
+        }
+    };
+
+    cmt::Event dataEvt;
+    std::size_t dataCounter = 0;
+    response->data() += sol * [&](Bytes data, bool done)
+    {
+        primitives::String dataStr = data.toString();
+
+        std::size_t localIdx{};
+        while(dataCounter < dataStr.size())
+        {
+            switch(dataCounter)
+            {
+            case 0:
+                EXPECT_EQ(dataStr[localIdx], 'x');
+                break;
+            case 1:
+                EXPECT_EQ(dataStr[localIdx], 'y');
+                break;
+            case 2:
+                EXPECT_EQ(dataStr[localIdx], 'z');
+                break;
+            default:
+                ADD_FAILURE();
+            }
+
+            ++localIdx;
+            ++dataCounter;
+            EXPECT_EQ(done, dataCounter == 3);
+            if(done)
+                dataEvt.raise();
+        }
+    };
+
+    request->firstLine(www::http::firstLine::Method::GET, "/", www::http::firstLine::Version::HTTP_1_1);
+    request->headers(
+                primitives::List<www::http::Header> {
+                    {www::http::header::KeyRecognized::Host, "localhost"},
+                    {"x-my-header", "x-my-value"},
+                }, true);
+    request->data("xyz", true);
+
+    poll::WaitableTimer<cmt::Event> tmr{std::chrono::seconds{1}};
+    tmr.start();
+
+    //cmt::wait(tmr || (firstLineEvt && headersEvt && dataEvt));
+    while(!tmr.raisableAndWaitable().isRaised())
+    {
+        if(!firstLineEvt.isRaised()) cmt::waitAny(tmr, firstLineEvt);
+        if(!headersEvt.isRaised()) cmt::waitAny(tmr, headersEvt);
+        if(!dataEvt.isRaised()) cmt::waitAny(tmr, dataEvt);
+    }
+
+    EXPECT_EQ(1, firstLineCounter);
+    EXPECT_EQ(2, headersCounter);
+    EXPECT_EQ(3, dataCounter);
 }
